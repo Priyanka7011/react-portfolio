f53b62454d9d0039f11e3f0633c87acfa5b12828
Priyanka7011 <pk3201134@gmail.com>
2020-05-30 04:23:30 +0530

    Updates

---
+++ precache-manifest.702be96a7d333784a652354a933768e6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c023e4c92a45466de07b12e40df54e30",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d9b6be648c907e5a5f30",
+    "url": "/react-portfolio/static/css/main.e4de6ddc.chunk.css"
+  },
+  {
+    "revision": "4606a0142a2dfa004563",
+    "url": "/react-portfolio/static/js/2.df7da057.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.df7da057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9b6be648c907e5a5f30",
+    "url": "/react-portfolio/static/js/main.c51bf6e5.chunk.js"
+  },
+  {
+    "revision": "be3cb1a3dfa81366eb8c",
+    "url": "/react-portfolio/static/js/runtime-main.5ca49836.js"
+  },
+  {
+    "revision": "a9436d74b1e58054145f83763d640ae0",
+    "url": "/react-portfolio/static/media/kirusa.a9436d74.png"
+  },
+  {
+    "revision": "f0efe0952cc9a80a6420ae92b7d1fc35",
+    "url": "/react-portfolio/static/media/picture.f0efe095.jpg"
+  },
+  {
+    "revision": "98365b996391c0e63fd99766c142d116",
+    "url": "/react-portfolio/static/media/purohit.98365b99.png"
+  }
+]);
